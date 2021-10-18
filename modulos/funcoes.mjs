f4b2ecf3076e9c6b7e8704431f45dadb18125f27
victor Farias <victorfarias.tv@gmail.com>
2021-10-18 09:55:18 -0300

    Curso de modulos finalizado

---
+++ modulos/funcoes.mjs
@@ -0,0 +1,17 @@
+const mostraIdade = (nome, idade) => {
+  return `A idade de ${nome} é ${idade}`;
+}
+
+const mostraCidade = (nome, cidade) => {
+  return `A cidade de ${nome} é ${cidade}`;
+}
+
+const mostraHobby = (nome, hobby) => {
+  return `O hobby de ${nome} é ${hobby}`;
+}
+
+export {
+  mostraIdade,
+  mostraCidade,
+  mostraHobby
+};
---
+++ modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Victor', 23));
+console.log(mostraCidade('Victor', 'São Paulo'));
+console.log(mostraHobby('Victor', 'Ver séries'));
